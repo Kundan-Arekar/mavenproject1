8885843796610308bad116fe27a89c709448347a
Kundan-Arekar <kundan_arekar@epam.com>
2024-03-19 09:07:42 +0530

    first commit

---
+++ src/test/java/Regression.java
@@ -0,0 +1,13 @@
+package RegressionTest;
+
+import org.testng.annotations.Test;
+
+public class Regression {
+
+    @Test
+    public void test() {
+        System.out.println("Hello, Regression!");
+
+    }
+
+}
---
+++ src/test/java/Smoke.java
@@ -0,0 +1,13 @@
+package SmokeTest;
+
+import org.testng.annotations.Test;
+
+public class Smoke {
+
+    @Test
+    public void test() {
+        System.out.println("Hello, smoke!");
+
+    }
+
+}
